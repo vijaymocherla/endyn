0def9dc212786c9b2e9a0b6613eede181ef36ab2
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2022-04-20 14:17:17 +0530

    changes in CISD methods

--- pyci/configint/cisd.py
+++ pyci/configint/cisd.py
@@ -10,16 +10,17 @@ import gc
 from multiprocessing import Pool
 from configint.bitstrings import bitDet, SlaterCondon
 from configint.csf import CSF
-from configint.csf import gen_singlet_doubles, gen_singlet_doubles
+from configint.csf import gen_singlet_singles, gen_singlet_doubles
 
 class CISD:
-    def __init__(orbinfo, mo_eps, mo_coeff, mo_erints, mo_edipoles):
+    def __init__(self, orbinfo, mo_eps, mo_coeff, mo_erints, mo_edipoles):
+        self.orbinfo = orbinfo
         self.mo_eps = mo_eps
         self.mo_coeff = mo_coeff
         self.mo_erints = mo_erints
         self.mo_edipoles = mo_edipoles
-        self.csf_list, self.exc_list = gen_csfs(orbinfo)
-        self.nCSFs = len(csf_list)
+        self.csf_list, self.exc_list = CISD.gen_csfs(self.orbinfo)
+        self.nCSFs = len(self.csf_list)
         self.SlaterCondonRule = SlaterCondon(mo_eps, mo_coeff, mo_erints).comp_hmatrix_elem
 
     
@@ -33,19 +34,18 @@ class CISD:
         occ_list = range(nocc-act_occ, nocc)
         vir_list = range(nocc, nocc+act_vir)
         refDet = bitDet(alpha_orblist=list(range(nocc)), beta_orblist=list(range(nocc)))
-        csf_list = [refDet]
+        csf_list = [CSF({refDet : 1.0})]
         exc_list = [(0)]
         # Singles
         if full_cis:
-            csf_list, exc_list = gen_singles_csf_list(refDet, range(nocc), range(nocc,nmo), csf_list, exc_list)
+            csf_list, exc_list = gen_singlet_singles(refDet, range(nocc), range(nocc,nmo), csf_list, exc_list)
         else:
-            csf_list, exc_list = gen_singles_csf_list(refDet, occ_list, vir_list, csf_list, exc_list)
+            csf_list, exc_list = gen_singlet_singles(refDet, occ_list, vir_list, csf_list, exc_list)
         # Doubles
-        csf_list, exc_list = gen_doubles_csf_list(refDet, occ_list, vir_list, csf_list, exc_list)
+        csf_list, exc_list = gen_singlet_doubles(refDet, occ_list, vir_list, csf_list, exc_list)
         return csf_list, exc_list
     
-    @staticmethod
-    def comp_cisd_hmatrix_elem(csf1, csf2):
+    def comp_cisd_hmatrix_elem(self, csf1, csf2):
         """Evaluates H-matrix elements using Slater-Condon rule
         """
         matrix_elem = sum([ci*cj*self.SlaterCondonRule(I,J) 
@@ -60,7 +60,7 @@ class CISD:
         try:
             for Q in range(P, self.nCSFs):
                 csf1, csf2 = self.csf_list[P], self.csf_list[Q]
-                row[Q] = CISD.comp_hcisd_matrix_elem(csf1, csf2)
+                row[Q] = self.comp_cisd_hmatrix_elem(csf1, csf2)
             return row, 1
         except :
             raise Exception("Something went wrong while computing row %i" % (P+1))
@@ -70,30 +70,29 @@ class CISD:
         """ Parallel computation HCISD
         """
         with Pool(processes=ncore) as pool:
-            async_object = pool.map_async(self.comp_cisd_matrix_row, range(self.nCSFs))
+            async_object = pool.map_async(self.comp_cisd_hmatrix_row, range(self.nCSFs))
             rows_data = async_object.get()
             pool.close()
             pool.join() 
         # checking if all process finished succesfully
         row_log = [rows_data[i][1] for i in range(len(rows_data))]
-        if sum(row_log) != int(len(self.nCSFs)):
+        if sum(row_log) != self.nCSFs:
             for idx, val in enumerate(row_log):
                 if val != 1:
                     print('Something went wrong while computing row %i' % (idx+1))
         else:
             print("All rows were computed successfully! \n")
-        HCISD = np.array(rows_data[:,0])
+        HCISD = np.array(rows_data[:][0])
         del rows_data
         gc.collect()
-        for P in range(nCSFs):
-            for Q in range(P, nCSFs):
+        for P in range(self.nCSFs):
+            for Q in range(P, self.nCSFs):
                 HCISD[Q,P] = HCISD[P,Q]
         return HCISD
     
-    def make_rdm1(self):
-        nel, nbf, nmo = orbinfo
+    def make_rdm1(self, active_space):
+        nel, nbf, nmo = self.orbinfo
         nocc, nvir = int(nel/2), int((nmo-nel)/2)
         act_occ, act_vir = active_space
         rdm1 = np.zeros((nmo, nmo))
-
         return rdm1  
