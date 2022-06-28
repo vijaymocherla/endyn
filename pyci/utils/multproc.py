0882f672a9d0a838e282d3bdb7410164743eef3c
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2022-06-28 09:12:04 +0530

    added pool_jobs func

---
+++ pyci/utils/multproc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+#   Author : Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
+#
+""" multproc.py
+Contains parallelisation routines writtein for pyci using
+multiprocessing library natively available in python.
+"""
+from multiprocessing import Pool
+
+# use better func name
+def pool_jobs(pfunc, arglist, ncore=4):
+    with Pool(processes=ncore) as pool:
+        async_object = pool.map_async(pfunc, arglist)
+        data = async_object.get()
+        pool.close()
+        pool.join()
+    return data
+
