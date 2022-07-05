947d7edb4fc61134950e2e68671649ba41a8aa1f
vijaymocherla <vijaysai.mocherla@gmail.com>
2022-07-05 15:53:01 +0530

    adding SO to dev

--- pyci/integrators/splitoperator.py
+++ pyci/integrators/splitoperator.py
@@ -1,19 +1,27 @@
+from pickletools import optimize
 import numpy as np 
 from time import perf_counter
 from opt_einsum import contract
 
-class ExactProp(object):
+class SplitOperator(object):
     """Given eigenvalues and eigen-vectors(in a certain basis), the methods of 
     exact_prop() aids in exact time-propagation.  
     """
-    def __init__(self, eigen_values, eigen_vectors, y0, time_params):
+    def __init__(self, eigen_values, eigen_vectors, field_func, y0, time_params):
         self.eigvals = eigen_values         # eigvals[i] => i-th eigenvalue
         self.eigvecs = eigen_vectors        # eigvecs[:,i] => i-th eigen-vector 
+        self.feild_func = field_func
         self.y0 = y0                                # initial state in CSF basis 
         self.y0_eigen = self.project_vec_eig(y0)   # initial state in EIGEN basis
         self.t0, self.tf, self.dt = time_params # time params
         self.y_list = [self.y0]
         self.t_list = [self.t0]
+
+    def project_matrix_eigbasis(self, matrix):
+        """Projects a matrix from CSF basis => EIGEN basis 
+        """
+        matrix_eig = contract('iA, AB, Bj -> ij', np.conjugate(self.eigvecs.T), matrix, self.eigvecs, optimize=True) 
+        return matrix_eig
 
     def project_vec_eig(self, y):
         """Projects y from CSF basis => EIGEN basis 
