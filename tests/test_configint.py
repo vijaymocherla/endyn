4271b793aef2a10a5c9e8778af340e1d54c8e275
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2022-07-13 09:26:58 +0530

    created meta and tests folders

---
+++ tests/test_configint.py
@@ -0,0 +1,10 @@
+import unittest
+from pyci.configint import rcisd
+
+class TestConfigint(unittest.TestCase):
+    
+    def testcase(self):
+        return 0
+
+if __name__ == "__main__":
+    unittest.main()
