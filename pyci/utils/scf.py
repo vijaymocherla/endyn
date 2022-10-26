6a8f5f43087d357e1ff59b75f6289119cf123024
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2022-10-26 23:50:40 +0530

    custom SCF subroutine for RHF with DIIS

--- pyci/utils/scf.py
+++ pyci/utils/scf.py
@@ -2,77 +2,80 @@
 #
 # Author : Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
 #
-""" Self-Consistent Field(SCF) procedure accelerated with Direct Inversion 
-    of the Iterative Subspace(DIIS)
+""" Self-Consistent Field(SCF) procedure accelerated 
+    with Direct Inversion of the Iterative Subspace(DIIS)
 """
+import os
 import numpy as np
 from pyci.utils import AOint
-### TO DO : 
-#    - Repurpose following code into an SCF function
-#    - Add options to control DIIS
-#    - Check future compatibility for EDIIS and other variational relaxation   
-# S = np.load('../data/h2o_oeints.npz')['overlap']
-# T = np.load('../data/h2o_oeints.npz')['kinetic']
-# V = np.load('../data/h2o_oeints.npz')['potential']
-# I = np.load('../data/h2o_erints.npz')['erints']
+from pyci.linalg import blas, lapack
+import os
 
-def orthonormalize(S, atol=1e-8):
-    # Diagonalise the Overlap matrix
+
+def orthonormalize(S):
+    # check_overlap = np.allclose(np.diag(S), np.ones(S.shape[0]))
+    # if not check_overlap:
+    #     raise Exception('Improper overlap matrix was given.')
     svals, svecs = np.linalg.eigh(S)
-    # Taking the inverse square root
-    X = np.linalg.inv(np.diag(np.sqrt(svals)))
-    # Using the eigen vector back transform from eigen basis to previous basis.
-    X = np.einsum('aI,IJ,Jb', svecs, X, svecs.T)
-    # Orthonormalising S
-    S_p = np.einsum('ij,jk,kl->il', X.T, S, X)
-    orthonormalised = np.allclose(S_p, np.eye(S.shape[0]), atol=atol)
-    if orthonormalised:
-        print("There is a chance for diagonalisation")
-    else:
-        raise Exception("There's something wrong with symmetric orthonormalisation")
+    X = lapack.dinv(np.diag(np.sqrt(svals)))
+    X = blas.dmul_mmm(svecs, X, svecs.T)
+    S_p = blas.dmul_mmm(X.T, S, X)
+    # Checking if orthonormalised
+    check_overlap = np.allclose(S_p, np.eye(S.shape[0]), atol=1e-08)
+    if not check_overlap:
+        raise Exception("There's something wrong with the transformation.")
     return S_p, X
 
-def get_density(H, X, nocc):
-    # Transforming the Fock matrix
-    F_p = np.einsum('ij,jk,kl->il', X.T, H, X)
-    # Diagonalising the Fock matrix eigenvalues and eigenvectors
-    e, C_p = np.linalg.eigh(F_p)
-    # Transform C_p back into AO basis
-    C = np.dot(X, C_p)
-    # slicing out occupied orbitals
-    C_occ = C[:, :nocc]
-    # Building density matrix from C_occ
-    D = np.einsum('pi,qi->pq', C_occ, C_occ, optimize=True)
-    return D
 
-def scf(S,T,V,I, orbinfo):
-    nocc, nmo = orbinfo
-    S_p, X = orthonormalize(S)
+def SCF(AOint, scfoptions={}):
+    options = {
+        'maxiter': 75,
+        'e_convergence': 1e-12,
+        'd_convergence': 1e-10,
+        'diis': True,
+        'diis_iter': 15,
+    }
+    options.update(scfoptions)
+    if not os.path.isfile(AOint.scratch+'ao_oeints.npz'):
+        AOint.save_ao_oeints()
+    if not os.path.isfile(AOint.scratch+'ao_erints.npz'):
+        AOint.save_ao_erints()
+    S, T, V = AOint.get_ao_oeints()
+    I = AOint.get_ao_erints()
     H = T + V
-    D = get_density(H, X, nocc)
-    # ==> SCF Iterations <==
-    # Maximum SCF iterations
-    MAXITER = 50
-    # Energy convergence criterion
-    E_conv = 1.0e-10
-    D_conv = 1.0e-8
-    # Pre-iteration energy declarations
-    SCF_E = 0.0
+    S_p, X = orthonormalize(S)
+    F_p = blas.dmul_mmm(X.T, H, X)
+    eps_p, C_p = np.linalg.eigh(F_p)
+    C = blas.dmul_mm(X, C_p)
+    nel = (AOint.wfn.nalpha() + AOint.wfn.nbeta())
+    nmo = AOint.wfn.nmo()
+    if nel % 2:
+        raise Exception('Open-Shell system passed RHF algorithm.')
+    ndocc = int(nel/2)
+    options['nmo'] = nmo
+    options['ndocc'] = ndocc
+    C_occ = C[:, :ndocc]
+    D = blas.dmul_mm(C_occ, C_occ.T)
+    MAXITER = options.get('maxiter')
+    # int(N) if you want to switch of DIIS after 'N' scf cycles
+    DIIS_ITER = options.get('diis_iter')
+    E_CONV = options.get('e_convergence')
+    D_CONV = options.get('e_convergence')
     E_old = 0.0
+    D_old = np.zeros(D.shape)
+    SCF_E = 0.0
+    # begin Iterations
+    print('==> Starting SCF Iterations <==\n')
     # Trial & Residual Vector Lists
-    diis_iter = MAXITER # int(N) if you want to switch of DIIS after 'N' scf cycles 
     F_list = []
     DIIS_RESID = []
     # ==> SCF Iterations w/ DIIS <==
-    print('==> Starting SCF Iterations <==\n')
-    # Begin Iterations
     for scf_iter in range(1, MAXITER + 1):
         # Build Fock matrix
         J = np.einsum('pqrs,rs->pq', I, D, optimize=True)
         K = np.einsum('prqs,rs->pq', I, D, optimize=True)
         F = H + 2*J - K
-        if scf_iter <= diis_iter:
-            #print('DIIS is ON \n')
+        if scf_iter <= DIIS_ITER:
             diis_bool = True
             # Build DIIS Residual
             diis_r = X.dot(F.dot(D).dot(S) - S.dot(D).dot(F)).dot(X)
@@ -80,7 +83,6 @@ def scf(S,T,V,I, orbinfo):
             F_list.append(F)
             DIIS_RESID.append(diis_r)
         else:
-            #print('DIIS is OFF \n')
             diis_bool = False
         # Compute RHF energy
         SCF_E = np.einsum('pq,pq->', (H + F), D, optimize=True)
@@ -89,14 +91,16 @@ def scf(S,T,V,I, orbinfo):
         if diis_bool:
             diis_str = '   DIIS is ON'
         else:
-            diis_str = '   DIIS is OFF'    
-        print(('SCF Iteration %3d: Energy = %4.16f dE = % 1.5E dRMS = %1.5E' % (scf_iter, SCF_E, dE, dRMS)) + diis_str )
-        
-        # SCF Converged?
-        if (abs(dE) < E_conv):
+            diis_str = '   DIIS is OFF'
+        print(('SCF Iteration %3d: Energy = %4.16f dE = % 1.5E dRMS = %1.5E' %
+               (scf_iter, SCF_E, dE, dRMS)) + diis_str)
+        # Checking if SCF Converged?
+        # if (abs(dE) <= E_CONV):
+        #     break
+        if np.allclose(D, D_old, rtol=D_CONV):
             break
         E_old = SCF_E
-        
+        D_old = D
         if scf_iter >= 2:
             # Build B matrix
             B_dim = len(F_list) + 1
@@ -106,31 +110,29 @@ def scf(S,T,V,I, orbinfo):
             B[-1, -1] = 0
             for i in range(len(F_list)):
                 for j in range(len(F_list)):
-                    B[i, j] = np.einsum('ij,ij->', DIIS_RESID[i], DIIS_RESID[j], optimize=True)
-    
-            # Build RHS of Pulay equation 
+                    B[i, j] = np.einsum('ij,ij->', DIIS_RESID[i],
+                                        DIIS_RESID[j], optimize=True)
+            # Build RHS of Pulay equation
             rhs = np.zeros((B_dim))
             rhs[-1] = -1
-            
             # Solve Pulay equation for c_i's with NumPy
-            coeff = np.linalg.solve(B, rhs)
-            
+            coeff = lapack.dsolve(B, rhs)
             # Build DIIS Fock matrix
             F = np.zeros_like(F)
             for x in range(coeff.shape[0] - 1):
                 F += coeff[x] * F_list[x]
-        
+
         # Compute new orbital guess with DIIS Fock matrix
-        F_p =  X.dot(F).dot(X)
+        F_p = X.dot(F).dot(X)
         e, C_p = np.linalg.eigh(F_p)
         C = X.dot(C_p)
+        ndocc = options.get('ndocc')
         C_occ = C[:, :ndocc]
         D = np.einsum('pi,qi->pq', C_occ, C_occ, optimize=True)
-        
         # MAXITER exceeded?
         if (scf_iter == MAXITER):
             raise Exception("Maximum number of SCF iterations exceeded.")
     # Post iterations
     print('\nSCF converged.')
-    print('Final RHF Energy: %.8f [Eh]' % SCF_E)
-    return SCF_E
+    print('Final RHF Energy: %.12f [Eh]' % SCF_E)
+    return SCF_E, C
