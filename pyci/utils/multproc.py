3ceabc773e4a70898384c31532a04cff1692bcbe
Sai Vijay Mocherla <vijaysai.mocherla@gmail.com>
2022-11-03 15:59:34 +0530

    added memmap load for .npz

--- pyci/utils/multproc.py
+++ pyci/utils/multproc.py
@@ -9,6 +9,7 @@ multiprocessing library natively available in python.
 from multiprocessing import Pool, Queue, RawArray
 from functools import partial
 import numpy as np
+from zipfile import ZipFile
 
 def pool_jobs(func, arglist, ncore, initializer, initargs):
     with Pool(processes=ncore, initializer=initializer, 
@@ -19,3 +20,19 @@ def pool_jobs(func, arglist, ncore, initializer, initargs):
         pool.join()
     return data
 
+
+def load_from_npz(npzfilename, array_name):
+    zf = ZipFile(npzfilename)
+    # figure out offset of .npy in .npz
+    info = zf.NameToInfo[array_name + '.npy']
+    assert info.compress_type == 0
+    zf.fp.seek(info.header_offset + len(info.FileHeader()) + 20)
+    # read .npy header
+    version = np.lib.format.read_magic(zf.fp)
+    np.lib.format._check_version(version)
+    shape, fortran_order, dtype = np.lib.format._read_array_header(zf.fp, version)
+    offset = zf.fp.tell()
+    # create memmap
+    return np.memmap(zf.filename, dtype=dtype, shape=shape,
+                     order='F' if fortran_order else 'C', mode='rb',
+                     offset=offset)
